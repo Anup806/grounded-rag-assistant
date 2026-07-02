a3a82eeb581efd961cbbabe5e1c8963a73370664
Anup806 <raianup806@gmail.com>
2026-07-03 00:19:27 +0545

    url of Qrant and SQLite changed

--- app/core/config.py
+++ app/core/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     QDRANT_URL: str = "http://localhost:6333"
     QDRANT_COLLECTION: str = "backend_rag_docs"
     REDIS_URL: str = "redis://localhost:6379"
-    SQLITE_URL: str = "sqlite:///./palm_mind.db"
+    SQLITE_URL: str = "sqlite:///./rag_backend.db"
     EMBEDDING_MODEL: str = "all-MiniLM-L6-v2"
     GROQ_MODEL: str = "llama-3.1-8b-instant"
     CHUNK_SIZE: int = 500
